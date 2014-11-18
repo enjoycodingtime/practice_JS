d9960b986bfd4f90edfebbdc879b0a1b76029c1e
zhangekehbg <itzhangke@gmail.com>
2014-11-18 21:54:51 +0800

    count_change

--- spec/ShuaGuaiSpec.js
+++ spec/ShuaGuaiSpec.js
@@ -8,6 +8,30 @@ describe("ShuaGuai", function() {
   it('一个函数，接受一个参数n，计算n的阶乘',function(){
     expect(factorial(4)).toEqual(24);
   });
-  
+  it('一个函数，接受一个参数n，实现菲波那契数列，1，2，3，5，8…',function(){
+     expect(fibonacci_number_column(5)).toEqual(8);
+  });
 });
+describe('一个函数A，接受两个参数x,y，如果y为零，返回0， 如果x为零返回2y，如果y=1则返回2。否则调用y-1做为新的y执行本流程得到的返回值作为另一个新的y，x-1再次执行本流程，A(x-1, A(x, y-1)) ',function(){
+  it('if y=0 return 0',function(){
+  	expect(A(1,0)).toEqual(0);
+  });
+  it('if x=0 return 2Y',function(){
+  	expect(A(0,1)).toEqual(2);
+  });
+  it('if y=1 return 2',function(){
+  	expect(A(1,1)).toEqual(2);
+  });
+});
+describe('找零钱',function(){
+   it('if n =1',function(){
+   	expect(count_change(1)).toEqual(1);
+   });
+   it('if n = 5',function(){
+   	expect(count_change(5)).toEqual(2);
+   });
+   it('if n = 11',function(){
+   	expect(count_change(11)).toEqual(4);
+   });
+})
 
--- src/shuaguai.js
+++ src/shuaguai.js
@@ -1,7 +1,6 @@
 function get_the_two_largest_number_together(a,b,c){
 	var my_array = [a,b,c];
 	my_array = my_array.sort(sortNumber);
-	console.log(my_array);
 	return (my_array[1]+my_array[2]);
 }
 function sortNumber(a,b){
@@ -11,9 +10,60 @@ function sortNumber(a,b){
 function two_number_together(a,b){
 	return a+b;
 }
+
 function factorial(n){
 	if(n==1){
 		return  1;
 	}
 	return factorial(n-1)*n;
-}
+};
+
+function fibonacci_number_column(n){
+	if(n == 1){
+		return 1;
+	};
+	if(n == 2){
+		return 2;
+	}
+	return fibonacci_number_column(n-2)+fibonacci_number_column(n-1);
+};
+
+function A(x,y){
+	if(y == 0){
+		return 0;
+	};
+	if(x == 0){
+		return 2*y;
+	};
+	if(y == 1){
+		return 2;
+	};
+	return A(x-1,A(x,y-1));
+};
+
+function count_change(amount){  
+  
+    return cc(amount, 5);  
+}  
+function cc(amount, kind_of_coins) {  
+    if(amount == 0){  
+        return 1;  
+    }else if(amount < 0||kind_of_coins == 0){  
+        return 0  
+    }else{  
+        return cc(amount, kind_of_coins - 1) + cc(amount-first_denomination(kind_of_coins), kind_of_coins)  
+    }  
+}  
+function first_denomination(kind_of_coins){  
+    if(kind_of_coins == 1){  
+        return 1;  
+    }else if(kind_of_coins == 2){  
+        return 5;  
+    }else if(kind_of_coins == 3){  
+        return 10;  
+    }else if(kind_of_coins == 4){  
+        return 25;  
+    }else if(kind_of_coins == 5){  
+        return 50;  
+    }  
+}  
